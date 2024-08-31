2288fe9dabc523ee120926fef7c24a8244f8fb33
Gerth123 <robingerth21@gmail.com>
2024-08-31 23:22:00 +0200

    Implemented the smooth scroll animation and the href for the components.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { ContactComponent } from './contact/contact.component'; 
+import { ContactComponent } from './contact/contact.component';
+import { AboutMeComponent } from './about-me/about-me.component'; 
+import { MySkillsComponent } from './my-skills/my-skills.component';
+import { ProjectsComponent } from './projects/projects.component';
 
 export const routes: Routes = [
-  { path: 'contact', component: ContactComponent }
+  { path: 'contact', component: ContactComponent },
+  { path: 'about-me', component: AboutMeComponent },
+  { path: 'my-skills', component: MySkillsComponent },
+  { path: 'projects', component: ProjectsComponent },
 ];
 
 @NgModule({
--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterModule, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-projects',
   standalone: true,
-  imports: [],
+  imports: [RouterOutlet, RouterModule],
   templateUrl: './projects.component.html',
   styleUrl: './projects.component.scss'
 })
