075a017659fe1ac343851decd23d76d718e9cebd
Gerth123 <robingerth21@gmail.com>
2024-09-16 22:26:06 +0200

    Did some responsiveness for the landingpage.

--- src/app/my-skills/my-skills.component.ts
+++ src/app/my-skills/my-skills.component.ts
@@ -1,25 +1,56 @@
-import { NgFor } from '@angular/common';
+import { NgClass, NgFor, NgIf } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-my-skills',
   standalone: true,
-  imports: [NgFor],
+  imports: [NgFor, NgIf, NgClass],
   templateUrl: './my-skills.component.html',
-  styleUrl: './my-skills.component.scss'
+  styleUrl: './my-skills.component.scss',
 })
 export class MySkillsComponent {
-  skills: { src: string; alt: string; name: string; }[] = [
+  skills: { src: string; alt: string; name: string }[] = [
     { src: '../../assets/icons/html.svg', alt: 'HTML', name: 'HTML' },
     { src: '../../assets/icons/css.svg', alt: 'CSS', name: 'CSS' },
-    { src: '../../assets/icons/javascript.svg', alt: 'Javascript', name: 'Javascript' },
-    { src: '../../assets/icons/typescript.svg', alt: 'Typescript', name: 'Typescript' },
+    {
+      src: '../../assets/icons/javascript.svg',
+      alt: 'Javascript',
+      name: 'Javascript',
+    },
+    {
+      src: '../../assets/icons/typescript.svg',
+      alt: 'Typescript',
+      name: 'Typescript',
+    },
     { src: '../../assets/icons/angular.svg', alt: 'Angular', name: 'Angular' },
-    { src: '../../assets/icons/firebase.svg', alt: 'Firebase', name: 'Firebase' },
+    {
+      src: '../../assets/icons/firebase.svg',
+      alt: 'Firebase',
+      name: 'Firebase',
+    },
     { src: '../../assets/icons/git.svg', alt: 'Git', name: 'Git' },
-    { src: '../../assets/icons/rest_api.svg', alt: 'Rest Api', name: 'Rest Api' },
+    {
+      src: '../../assets/icons/rest_api.svg',
+      alt: 'Rest Api',
+      name: 'Rest Api',
+    },
     { src: '../../assets/icons/scrum.svg', alt: 'Scrum', name: 'Scrum' },
-    { src: '../../assets/icons/material_design.svg', alt: 'Material Design', name: 'Material Design' },
-    { src: '../../assets/icons/continually_learning.svg', alt: 'Continually Learning', name: 'Continually Learning' },
+    {
+      src: '../../assets/icons/material_design.svg',
+      alt: 'Material Design',
+      name: 'Material Design',
+    },
   ];
+
+  isHovered: boolean = false;
+
+  constructor() {}
+
+  showSpecialInterest() {
+    this.isHovered = true;
+  }
+
+  hideSpecialInterest() {
+    this.isHovered = false;  
+  }
 }
