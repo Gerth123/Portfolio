b33adf30768f01d74c7dd366a6801ad2924de3b1
Gerth123 <robingerth21@gmail.com>
2024-09-26 01:11:43 +0200

    Implemented the translator also for the privacy policy and legal notice.

--- src/app/legal-notice/legal-notice.component.ts
+++ src/app/legal-notice/legal-notice.component.ts
@@ -1,13 +1,21 @@
 import { Component } from '@angular/core';
 import { ShadowsComponent } from '../shadows/shadows.component';
+import { LegalNoticeGermanComponent } from '../legal-notice-german/legal-notice-german.component';
+import { LanguageService } from '../services/language.service';
+import { CommonModule, NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-legal-notice',
   standalone: true,
-  imports: [ShadowsComponent],
+  imports: [ShadowsComponent, LegalNoticeGermanComponent, CommonModule],
   templateUrl: './legal-notice.component.html',
-  styleUrl: './legal-notice.component.scss'
+  styleUrl: './legal-notice.component.scss',
 })
 export class LegalNoticeComponent {
-  
+  language: string = 'en';
+  constructor(private languageService: LanguageService) {
+    setInterval(() => {
+      this.language = this.languageService.currentLanguage;
+    }, 1000);
+  }
 }
--- src/app/privacy-policy/privacy-policy.component.ts
+++ src/app/privacy-policy/privacy-policy.component.ts
@@ -1,13 +1,21 @@
 import { Component } from '@angular/core';
 import { ShadowsComponent } from '../shadows/shadows.component';
+import { PrivacyPolicyGermanComponent } from '../privacy-policy-german/privacy-policy-german.component';
+import { LanguageService } from '../services/language.service';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-privacy-policy',
   standalone: true,
-  imports: [ShadowsComponent],
+  imports: [ShadowsComponent, PrivacyPolicyGermanComponent, NgIf],
   templateUrl: './privacy-policy.component.html',
-  styleUrl: './privacy-policy.component.scss'
+  styleUrl: './privacy-policy.component.scss',
 })
 export class PrivacyPolicyComponent {
-
+  language: string = 'en';
+  constructor(private languageService: LanguageService) {
+    setInterval(() => {
+      this.language = this.languageService.currentLanguage;
+    }, 1000);
+  }
 }
